efc67159c4aebdfe80ef5506c07d00f6050c3a73
Cian Jinks <cjinks99@gmail.com>
2020-05-31 15:56:38 +0100

    Simplified camera code

--- src/main/java/Camera.java
+++ src/main/java/Camera.java
@@ -11,7 +11,6 @@ import java.nio.ByteBuffer;
 
 public class Camera {
     private Matrix4f mvp, projection, view;
-    private Vector3f position = new Vector3f(0.0f, 0.0f, 0.0f);
     private float width, height;
     private float zoom = 1.0f;
     private float baseSpeed = 0.0125f;
@@ -58,27 +57,12 @@ public class Camera {
     }
 
     public void zoom(float zoomAmount) {
-        Vector3f oldPosition = new Vector3f(position.x, position.y, position.z);
-        move(new Vector3f(-position.x, -position.y, -position.z));
-        projection.scaleXY(zoomAmount, zoomAmount);
-        move(oldPosition);
+        projection.scaleLocal(zoomAmount, zoomAmount, 0f);
         zoom = zoom * zoomAmount;
-        System.out.println(zoom);
-        System.out.println(currentSpeed);
-    }
-
-    public float getZoomAmount() {
-        return zoom;
-    }
-
-    public void setPosition(Vector3f position) {
-        this.position = position;
-        recalculateMVP();
     }
 
     public void move(Vector3f distance) {
         view = new Matrix4f().translate(distance).invert();
-        position.add(distance);
         recalculateMVP();
         updateSpeed();
     }
@@ -87,20 +71,8 @@ public class Camera {
         currentSpeed = baseSpeed / zoom;
     }
 
-    public Vector3f getPosition() {
-        return this.position;
-    }
-
-    public void setSpeed(float speed) {
-        this.currentSpeed = speed;
-    }
-
     public float getSpeed() {
         return this.currentSpeed;
-    }
-
-    public float getBaseSpeed() {
-        return this.baseSpeed;
     }
 
     public Matrix4f getMVP() {
